26a2997c0633b11fc835a127aebaa7de2fd1e451
Raymond Nguyen <rayknguyen@gmail.com>
2020-07-25 14:20:05 -0700

    Add test for tweet

--- test_app.py
+++ test_app.py
@@ -1,5 +1,6 @@
 import json
 from app import app
+import random
 
 test_browser = app.test_client()
 
@@ -60,3 +61,16 @@ def test_it_returns_member_details():
     assert 'district' in response_data.get('results')[0]
     assert 'website' in response_data.get('results')[0]
     assert 'contact_form_url' in response_data.get('results')[0]
+
+    def test_it_sends_tweet_to_microservice():
+        f = ['cool', 'great', 'awesome', 'chill', 'dude', 'yes', 'yazy']
+        for i in range(99999):
+            rand_num = random.choice(i, end=', ')
+        rand = random.choice(f)
+        global test_browser
+        handle = 'smj289'
+        message = f'hello {rand} steven{rand_num}'
+        response = test_browser.get(f'/tweet?{handle}&{message}')
+        response_data = json.loads(response.data)
+
+        assert 'message sent' == response_data
