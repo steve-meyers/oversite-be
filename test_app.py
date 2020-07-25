88ab5b8b60b0e70ee22916d88f44263bb21d840f
Steven Meyers <sjjmeyers@gmail.com>
2020-07-25 12:19:51 -0600

    Rename test file and add additional member tests

---
+++ test_app.py
@@ -0,0 +1,48 @@
+import json
+from app import app
+
+test_browser = app.test_client()
+
+def test_index_page():
+    global test_browser
+    response = test_browser.get('/')
+    assert response.status_code == 200
+
+def test_it_returns_users():
+    global test_browser
+    response = test_browser.get('/users')
+    assert response.status_code == 200
+    assert type(json.loads(response.data)) is list
+
+def test_it_returns_members_by_state():
+    global test_browser
+    state = 'co'
+    response = test_browser.get(f'/members_by_state/{state}')
+    assert response.status_code == 200
+    assert type(json.loads(response.data)) is dict
+    assert type(json.loads(response.data)['results'][0]['senate']) is list
+    assert type(json.loads(response.data)['results'][1]['house']) is list
+
+def test_it_returns_member_details():
+    global test_browser
+    member_id = 'G000562'
+    response = test_browser.get(f'/member/{member_id}')
+    response_data = json.loads(response.data)
+    assert response.status_code == 200
+    assert type(response_data) is dict
+    assert 'results' in response_data
+    assert 'id' in response_data.get('results')[0]
+    assert 'first_name' in response_data.get('results')[0]
+    assert 'last_name' in response_data.get('results')[0]
+    assert 'role' in response_data.get('results')[0]
+    assert 'phone' in response_data.get('results')[0]
+    assert 'address' in response_data.get('results')[0]
+    assert 'twitter' in response_data.get('results')[0]
+    assert 'youtube' in response_data.get('results')[0]
+    assert 'facebook' in response_data.get('results')[0]
+    assert 'party' in response_data.get('results')[0]
+    assert 'chamber' in response_data.get('results')[0]
+    assert 'state' in response_data.get('results')[0]
+    assert 'district' in response_data.get('results')[0]
+    assert 'website' in response_data.get('results')[0]
+    assert 'contact_form_url' in response_data.get('results')[0]
