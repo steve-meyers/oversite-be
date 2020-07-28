3f453856fd6e66eb30a2703416329ea2238d5518
Steven Meyers <sjjmeyers@gmail.com>
2020-07-27 23:44:23 -0600

    Refactor PropublicaService

--- services/propublica.py
+++ services/propublica.py
@@ -1,33 +1,43 @@
 import os
 import requests
 import json
-import code
 import dotenv
 
 dotenv.load_dotenv()
 
 class PropublicaService:
-
-    def conn():
-        headers = {'X-API-Key': os.getenv('PROP_API')}
-
+    @staticmethod
     def member_details(id_):
-        headers = {'X-API-Key': os.getenv('PROP_API')}
-        URL = f'https://api.propublica.org/congress/v1/members/{id_}.json'
-        return (requests.get(URL, headers=headers).json())['results'][0]
+        endpoint = f'{id_}.json'
+        url = PropublicaService.__base_url() + endpoint
+        headers = PropublicaService.__headers()
+        return (requests.get(url, headers = headers).json())['results'][0]
 
+    @staticmethod
     def senators_by_state(state_):
-        headers = {'X-API-Key': os.getenv('PROP_API')}
-        URL = f'https://api.propublica.org/congress/v1/members/senate/{state_}/current.json'
-        return (requests.get(URL, headers=headers).json())['results']
+        endpoint = f'senate/{state_}/current.json'
+        url = PropublicaService.__base_url() + endpoint
+        headers = PropublicaService.__headers()
+        return (requests.get(url, headers = headers).json())['results']
 
+    @staticmethod
     def reps_by_state(state_):
-        headers = {'X-API-Key': os.getenv('PROP_API')}
-        URL = f'https://api.propublica.org/congress/v1/members/house/{state_}/current.json'
-        return (requests.get(URL, headers=headers).json())['results']
+        endpoint = f'house/{state_}/current.json'
+        url = PropublicaService.__base_url() + endpoint
+        headers = PropublicaService.__headers()
+        return (requests.get(url, headers = headers).json())['results']
 
+    @staticmethod
     def reps_by_district(state_, district_):
-        headers = {'X-API-Key': os.getenv('PROP_API')}
-        URL = f'https://api.propublica.org/congress/v1/members/house/{state_}/{district_}/current.json'
-        return (requests.get(URL, headers=headers).json())['results']
+        endpoint = f'house/{state_}/{district_}/current.json'
+        url = PropublicaService.__base_url() + endpoint
+        headers = PropublicaService.__headers()
+        return (requests.get(url, headers = headers).json())['results']
 
+    @staticmethod
+    def __headers():
+        return {'X-API-Key': os.getenv('PROP_API')}
+
+    @staticmethod
+    def __base_url():
+        return 'https://api.propublica.org/congress/v1/members/'
